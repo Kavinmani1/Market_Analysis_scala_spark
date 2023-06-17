572c641eded9c92645387a0780951e592245f5b7
kavin <kavin.mani2000@gmail.com>
2023-06-17 11:43:29 +0530

    committed successfully

---
+++ src/main/scala/Marketing_Analysis.scala
@@ -0,0 +1,13 @@
+import org.apache.log4j.Logger
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.sql.types.StringType
+
+object Marketing_Analysis {
+  @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
